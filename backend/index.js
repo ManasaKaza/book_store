99bfac134ffe4ba928bf4012e2b8d3ce0fa06a08
prasanna <lakshmiprasannakusumanchi@gmail.com>
2024-08-11 19:30:29 +0530

    deploying

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 require('dotenv').config();
 require('./models/db');
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,13 +13,5 @@ app.use(require('helmet')());
 app.use('/api/books', require('./routes/books'));
 app.use('/api/user', require('./routes/user'));
 
-if (process.env.NODE_ENV === 'production') {
-
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
--- backend/models/db.js
+++ backend/models/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 // Allow Promises
 mongoose.Promise = global.Promise;
 // Connection
-mongoose.connect('mongodb+srv://student_user:oCEQTkO20V0DGTN3@cluster0.gfrftal.mongodb.net/book_store')
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Connected to the database!'))
   .catch(err => console.error('Error with the database!', err));  
