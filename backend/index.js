1051d55a75a92e4e8b0ee695dba9d2eaf5824103
prasanna <lakshmiprasannakusumanchi@gmail.com>
2024-08-11 23:20:33 +0530

    deploying

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');  
+const path = require('path');
 require('dotenv').config();
 require('./models/db');
 const app = express();
@@ -14,16 +14,5 @@ app.use(require('helmet')());
 app.use('/api/books', require('./routes/books'));
 app.use('/api/user', require('./routes/user'));
 
-// Serve static files in development
-if (process.env.NODE_ENV === 'development') {
-    app.use(express.static(path.join(__dirname, 'frontend')));
-}
-
-// For production, handle serving the client-side files differently
-if (process.env.NODE_ENV === 'production') {
-    // Handle serving client-side files from a CDN or other static file server
-    // Example:
-    // app.use(express.static(path.join(__dirname, 'frontend')));
-}
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
